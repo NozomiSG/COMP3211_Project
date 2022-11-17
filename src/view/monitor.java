4cedff8c2dac0d670ff2e82c4659bc49392fed6d
NozomiSG <zgy.peter@gmail.com>
2022-11-18 01:12:13 +0800

    feat: mjjj changeView

--- src/view/monitor.java
+++ src/view/monitor.java
@@ -10,13 +10,7 @@ public class monitor {
         System.out.println("==========================");
         for (int i=0; i<9; i++) {
             for (int j=0; j<7; j++) {
-                if(currentBoard[i][j].getAnimal()!=null && currentBoard[i][j].getAnimal().getSide()==0){
-                    System.out.format("\33[44;1m"+currentBoard[i][j].printSquare()+"\033[m");
-                }else if(currentBoard[i][j].getAnimal()!=null && currentBoard[i][j].getAnimal().getSide()==1)
-                    System.out.format("\33[46;1m"+currentBoard[i][j].printSquare()+"\033[m");
-                else{
-                    System.out.format("\33[47m"+currentBoard[i][j].printSquare()+"\033[m");
-                }
+                System.out.print(currentBoard[i][j].printSquare());
             }
             System.out.println();
         }
@@ -28,9 +22,42 @@ public class monitor {
             System.out.println("Warning: " + warn);
     } //print warning message when users try to do invalid action
 
-    public static void printWelcome() {} //print welcome message
+    public static void printBye() {
+        System.out.println(" ____             _ ");
+        System.out.println("| __ ) _   _  ___| |");
+        System.out.println("|  _ \\| | | |/ _ \\ |");
+        System.out.println("| |_) | |_| |  __/_|");
+        System.out.println("|____/ \\__, |\\___(_)");
+        System.out.println("       |___/        ");
+    }
 
-    public static void printManual() {} // print help message including move and manual commands
+    public static void printWelcome() {
+        System.out.print("__        __   _   \n\\ \\      / /__| | ___ ___  _ __ ___   ___ \n \\ \\ /\\ / / _ \\ |/ __/ _ \\| '_ ` _ \\ / _ \\\n  \\ V  V /  __/ | (_| (_) | | | | | |  __/\n   \\_/\\_/ \\___|_|\\___\\___/|_| |_| |_|\\___|\n");
+        System.out.println("==========================");
+        System.out.println("Welcome to Jungle game!");
+    } //print welcome message
+
+    public static void printManual() {
+        System.out.println();
+        System.out.println("This is a user manual. Enter \"help\" if you want to read it again.");
+        System.out.println();
+        System.out.println("instructions:");
+        System.out.println();
+        System.out.println("\t[number][direction] -move certain pieces in given direction");
+        System.out.println();
+        System.out.println("\t\tnumber: 1-鼠|2-貓|3-狗|4-狼|5-豹|6-虎|7-獅|8-象");
+        System.out.println();
+        System.out.println("\t\tdirection: w-UP|a-LEFT|s-DOWN|d-RIGHT");
+        System.out.println();
+        System.out.println("\t\t[e.g. “1s”-rat go down one step]");
+        System.out.println();
+        System.out.println("\texit -quit the game");
+        System.out.println();
+        System.out.println("\thelp -print the user manual");
+        System.out.println();
+        System.out.println("\trestart -restart the game");
+        System.out.println();
+    } // print help message including move and manual commands
 
     public static void noticeToMove(int side) {
         if (side == 0)
